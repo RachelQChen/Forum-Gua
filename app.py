8f9e029979a8f24e3e4de7ee47ff45447c305b0c
RachelQ1103 <chen_ruiqi1006@126.com>
2016-06-17 11:14:09 +0800

    3. 增加板块帖子路由，model-post，post.html

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import url_for
 from flask import request
 
 from models import Channel
+from models import Post
 
 app = Flask(__name__)
 
@@ -16,24 +17,46 @@ def index():
 
 @app.route('/channel/list')
 def channels():
-    channel_list = Channel.query.all()
-    return render_template('channels.html', channels=channel_list)
+    channels = Channel.query.all()
+    c_rows = []
+    for c in channels:
+        cr = c.channel_row()
+        c_rows.append(cr)
+    return render_template('channels.html', channels=c_rows)
 
 
-@app.route('/channel')
-def channel_view():
-    return render_template('new_channel.html')
-
-
-@app.route('/channel/new', methods=['POST'])
+@app.route('/channel/new', methods=['GET','POST'])
 def channel_new():
     c = Channel(request.form)
     c.save()
-    return redirect(url_for('channel_view'))
+    print('channel_new: ', c)
+    return render_template('new_channel.html')
+
+
+@app.route('/channel/<id>')
+def channel_view(id):
+    c = Channel.query.filter_by(id=id).first()
+    print('channel view: ', c)
+    plist = c.post_list()
+    print('post list: ', plist)
+    return render_template('channel.html', channel=c, posts=plist)
+
+
+@app.route('/post/new', methods=['POST'])
+def post_new():
+    p = Post(request.form)
+    p.save()
+    print('post_new: ', p)
+    cid = p.channel_id
+    print('Post_new channel_id', cid)
+    return redirect(url_for('channel_view', id=cid))
+
+@app.route('/post/<id>')
+def post_view(id):
+    p = Post.query.filter_by(id=id).first()
+    return render_template('post.html', post=p)
 
 
 if __name__ == '__main__':
     host = '0.0.0.0'
     app.run(host=host, debug=True)
-
-
--- models.py
+++ models.py
@@ -5,7 +5,6 @@ from sqlalchemy import sql
 import time
 import shutil
 
-
 db_path = 'models.db'
 app = Flask(__name__)
 app.secret_key = 'asdjf1923'
@@ -14,40 +13,64 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(db_path)
 db = SQLAlchemy(app)
 
 
-class Channel(db.Model):
+class Model(object):
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        properties = ('{0} = {1}'.format(k, v) for k, v in self.__dict__.items())
+        return u'\n<{0}:\n  {1}\n'.format(class_name, '\n   '.join(properties))
+
+
+class Channel(db.Model, Model):
     __tablename__ = 'channel'
     id = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String())
+    name = db.Column(db.String())
     created_time = db.Column(db.DateTime(timezone=True), default=sql.func.now())
 
     def __init__(self, form):
         # init 里 get 和 验证
-        self.link = form.get('link', '')
-
-    def __repr__(self):
-        return u'<{} {} {}>'.format(self.__class__.__name__, self.id, self.link)
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
+        self.name = form.get('name', '')
 
 
-class Problem(db.Model):
-    __tablename__ = 'problem'
+    def channel_row(self):
+        cr = {
+            'id': self.id,
+            'link': '<a href="/channel/{}">{}</a>'.format(self.id, self.name),
+            'time': self.created_time,
+        }
+        return cr
+
+    def post_list(self):
+        posts = Post.query.filter_by(channel_id=self.id).all()
+        plist = []
+        for p in posts:
+            plist.append(p.post_row())
+        return plist
+
+
+class Post(db.Model, Model):
+    __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String())
-    timestamp = db.Column(db.DateTime(timezone=True), default=sql.func.now())
+    body = db.Column(db.String())
+    channel_id = db.Column(db.String())
+    created_time = db.Column(db.DateTime(timezone=True), default=sql.func.now())
 
     def __init__(self, form):
         # init 里 get 和 验证
-        self.link = form.get('link', '')
+        self.body = form.get('body', '')
+        self.channel_id = form.get('channel_id', '')
 
-    def __repr__(self):
-        return u'<{} {} {}>'.format(self.__class__.__name__, self.id, self.link)
 
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
+    def post_row(self):
+        plink = '<a href="/post/{}">{}</a>'.format(self.id, self.body)
+        pr = {
+            'link': plink,
+            'time': self.created_time,
+        }
+        return pr
 
 
 def backup_db():
