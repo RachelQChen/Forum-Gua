72d2a0f25c157dd5d47380eb5d24dcb432e55c48
RachelQ1103 <chen_ruiqi1006@126.com>
2016-06-27 18:29:21 +0800

    49.完善基本功能，界面。

--- rLog.py
+++ rLog.py
@@ -3,5 +3,18 @@ import time
 def log(*args):
     t = time.time()
     formated_time = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(t))
+    # time_str_list = []
+    # for t in formated_time:
+    #     time_str_list.append(str(t))
+    # time_str = ''.join(time_str_list)
+    # arg_list = []
+    # for arg in args:
+    #     arg_list.append(str(arg))
+    # arg_str = '|'.join(arg_list)
+    # with open('log.txt', 'w') as f:
+    #     f.write(time_str + ' ' + arg_str + '\r\n')
     print(formated_time, *args)
+    # print(time_str + ' ' + arg_str + '\r\n')
+
+log(1,2,3)
 
