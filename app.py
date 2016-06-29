3e270e179225575fabfab73218d3a475b481a048
RachelQ1103 <chen_ruiqi1006@126.com>
2016-06-29 18:44:19 +0800

    62.app的jinjia增加时间过滤函数

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ from decorators import current_user
 from decorators import is_administrator
 from decorators import is_current_user
 
+import time
+
 app = Flask(__name__)
 
 app.secret_key = 'asdjf1923'
@@ -356,6 +358,56 @@ def user_update(user_id):
         abort(401)
 
 
+@app.template_filter('formatted_time')
+def formatted_time(timestamp):
+    time_format = '%Y/%m/%d %H:%M:%S'
+    t = time.localtime(timestamp)
+    ft = time.strftime(time_format, t)
+    return ft
+
+@app.template_filter('short_time')
+def short_time(timestamp):
+    time_format = '%m/%d %H:%M'
+    t = time.localtime(timestamp)
+    ft = time.strftime(time_format, t)
+    return ft
+
+@app.template_filter('from_now')
+def from_now(timestamp):
+    now = int(time.time())
+    from_now = now - timestamp
+    a_minute = 60
+    an_hour = 60 * 60
+    a_day = 60 * 60 * 24
+    a_week = 60 * 60 * 24 * 7
+    a_month = 60 * 60 * 24 * 30
+    a_year = 60 * 60 * 24 * 365
+    in_an_hour = now - an_hour
+    in_a_day = now - a_day
+    in_a_week = now - a_week
+    in_a_month = now - a_month
+    in_a_year = now - a_year
+    if timestamp < in_an_hour:
+        from_now_int = int(from_now / a_minute)
+        from_now_str = '{} 分钟前'.format(from_now_int)
+    elif timestamp < in_a_day:
+        from_now_int = int(from_now / an_hour)
+        from_now_str = '{} 小时前'.format(from_now_int)
+    elif timestamp < in_a_week:
+        from_now_int = int(from_now / a_day)
+        from_now_str = '{} 天前'.format(from_now_int)
+    elif timestamp < in_a_month:
+        from_now_int = int(from_now / a_week)
+        from_now_str = '{} 周前'.format(from_now_int)
+    elif timestamp < in_a_year:
+        from_now_int = int(from_now / a_month)
+        from_now_str = '{} 月前'.format(from_now_int)
+    else:
+        from_now_int = int(from_now / a_year)
+        from_now_str = '{} 年前'.format(from_now_int)
+    return from_now_str
+
+
 @app.context_processor
 def args_for_base():
     """Add args to base.html"""
@@ -364,6 +416,7 @@ def args_for_base():
     c_rows = []
     if user is not None:
         role = user.role
+
         channels = role.channels
         for c in channels:
             cr = c.channel_row()
